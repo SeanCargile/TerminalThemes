76b3eeb40630a450dcccec0d7ee3507b893c77ab
SeanCargile <59779959+SeanCargile@users.noreply.github.com>
2020-01-11 17:52:02 -0600

    Update OrangeTerminal.js

--- OrangeTerminal.js
+++ OrangeTerminal.js
@@ -1,29 +1,5 @@
 #Orange Terminal#
-
-black       = '#040201';
-red         = '#B75618'; // red
-green       = '#EC6E1A'; // green
-yellow      = '#904314'; // yellow
-blue        = '#5B2B0E'; // blue
-magenta     = '#BE5918'; // pink
-cyan        = '#6F3410'; // cyan
-white       = '#C75D19'; // light gray
-lightBlack  = '#EC6E1A'; // medium gray
-lightRed    = '#EC6E1A'; // red
-lightGreen  = '#904314'; // green
-lightYellow = '#7E3B12'; // yellow
-lightBlue   = '#984714'; // blue
-lightMagenta= '#D4631A'; // pink
-lightCyan   = '#FB751E'; // cyan
-lightWhite  = '#FF7A1F'; // white
-
-t.prefs_.set('color-palette-overrides',
-                 [ black , red     , green  , yellow,
-                  blue     , magenta , cyan   , white,
-                  lightBlack   , lightRed  , lightGreen , lightYellow,
-                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
-
+t.prefs_.set('color-palette-overrides',['#040201', '#B75618', '#EC6E1A', '#904314', '#5B2B0E', '#BE5918', '#6F3410', '#C75D19', '#EC6E1A', '#EC6E1A', '#904314', '#7E3B12', '#984714', '#D4631A', '#FB751E', '#FF7A1F']);
 t.prefs_.set('cursor-color', 'rgba(182, 85, 24, 0.5)');
 t.prefs_.set('foreground-color', '#EC6E1A');
 t.prefs_.set('background-color', '#070301');
-
