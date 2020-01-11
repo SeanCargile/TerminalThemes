f1f0cb2d25847105e3312b8fedd1cf589aa4a3ef
SeanCargile <59779959+SeanCargile@users.noreply.github.com>
2020-01-11 17:39:29 -0600

    Add files via upload

---
+++ OrangeTerminal.js
@@ -0,0 +1,29 @@
+#Orange Terminal#
+
+black       = '#040201';
+red         = '#B75618'; // red
+green       = '#EC6E1A'; // green
+yellow      = '#904314'; // yellow
+blue        = '#5B2B0E'; // blue
+magenta     = '#BE5918'; // pink
+cyan        = '#6F3410'; // cyan
+white       = '#C75D19'; // light gray
+lightBlack  = '#EC6E1A'; // medium gray
+lightRed    = '#EC6E1A'; // red
+lightGreen  = '#904314'; // green
+lightYellow = '#7E3B12'; // yellow
+lightBlue   = '#984714'; // blue
+lightMagenta= '#D4631A'; // pink
+lightCyan   = '#FB751E'; // cyan
+lightWhite  = '#FF7A1F'; // white
+
+t.prefs_.set('color-palette-overrides',
+                 [ black , red     , green  , yellow,
+                  blue     , magenta , cyan   , white,
+                  lightBlack   , lightRed  , lightGreen , lightYellow,
+                  lightBlue    , lightMagenta  , lightCyan  , lightWhite ]);
+
+t.prefs_.set('cursor-color', 'rgba(182, 85, 24, 0.5)');
+t.prefs_.set('foreground-color', '#EC6E1A');
+t.prefs_.set('background-color', '#070301');
+
